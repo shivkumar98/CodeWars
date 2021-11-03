1c283decd5ab99dd1000b6cb541db8829d3f935d
unknown <shiv_kumar.98@outlook.com>
2021-11-03 12:57:55 +0000

    finished VowelCount problem

---
+++ src/VowelCount/app.java
@@ -0,0 +1,27 @@
+package VowelCount;
+
+public class app {
+	
+	 public static int getCount(String str) {
+		 int vowelsCount = 0;
+		String[] sub =str.split("");
+	  int sum = 0;
+		for (var j =0;j<sub.length;j++) {
+			System.out.println(sub[j]);
+			if (sub[j].contains("a") || sub[j].contains("e") ||sub[j].contains("i")||sub[j].contains("o")||sub[j].contains("u"))
+					{
+      sum = sum + 1;
+    }
+		}
+		return	sum;
+	 }
+	 
+	 public static void main(String[] args) {
+		 
+		 
+		 System.out.println(getCount("hdsafo;bnaed"));
+		
+		
+	}
+
+}
