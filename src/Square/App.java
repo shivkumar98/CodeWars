7bfc3f748351ea47ce840e88da9aa608731c73c5
unknown <shiv_kumar.98@outlook.com>
2021-11-11 12:06:25 +0000

    completed square root problem

---
+++ src/Square/App.java
@@ -0,0 +1,18 @@
+package Square;
+
+public class App {
+	
+	public static boolean isSquare(int n) {        
+		if (Math.ceil(Math.sqrt(n))*Math.ceil(Math.sqrt(n))) {
+			return true;
+		} else {
+			return false;
+		}
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
